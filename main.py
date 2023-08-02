12e9b7a4dfcd45a7524a3a6e139e57e0495a73d5
Rex Coleman <rexcoleman@gmail.com>
2023-08-02 16:56:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from art import logo
+
+#Calculator
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+
+# def operation(input1, input2):
+
+num_inputs = []
+operation_inputs = []
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+}
+
+continue_calculating = "y"
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number? "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2) 
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+    
+        if input(f"Type 'y' to continue calculating with {answer}. Type 'n' to exit.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
